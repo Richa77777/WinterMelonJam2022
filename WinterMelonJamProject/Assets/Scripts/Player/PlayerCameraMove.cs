2da576a75c1a1e0abf63a95f90e82f9f1f6e5d39
Richa77777 <121635433+Richa77777@users.noreply.github.com>
2022-12-30 15:25:06 +0300

    Player Animations
    Added Player Animations

---
+++ WinterMelonJamProject/Assets/Scripts/Player/PlayerCameraMove.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerCameraMove : MonoBehaviour
+    {
+        private GameObject _target;
+        private Camera _camera;
+
+        [Range(0.0f, 5.0f)]
+        [SerializeField] private float _cameraSpeed;
+
+        private void Awake()
+        {
+            _target = gameObject;
+            _camera = Camera.main;
+        }
+
+        private void Start()
+        {
+            _camera.transform.localPosition = new Vector3(_target.transform.localPosition.x, _target.transform.localPosition.y, _camera.transform.localPosition.z);
+        }
+
+        private void Update()
+        {
+            CameraMove();
+        }
+
+        private void CameraMove()
+        {
+            Vector3 direction = _target.transform.localPosition;
+            direction.z = _camera.transform.localPosition.z;
+
+            _camera.transform.localPosition = Vector3.Lerp(_camera.transform.localPosition, direction, _cameraSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ WinterMelonJamProject/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMove : MonoBehaviour
+    {
+        public enum PlayerRotates
+        {
+            Up,
+            Down,
+            Right,
+            Left
+        }
+
+        private PlayerRotates _playerRotate = PlayerRotates.Down;
+
+        public PlayerRotates PlayerRotate { get { return _playerRotate; } }
+
+        [Range(0.0f, 5.0f)]
+        [SerializeField] private float _normalSpeed;
+        private float _currentSpeed;
+
+        private Rigidbody2D _rigidbody;
+        private Animator _animator;
+
+        float _horizontalAxis;
+        float _verticalAxis;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+            _currentSpeed = _normalSpeed;
+        }
+
+        private void Update()
+        {
+            if (_horizontalAxis != 0 || _verticalAxis != 0)
+            {
+                _animator.SetBool("isWalking", true);
+                _animator.SetFloat("Horizontal", _horizontalAxis);
+                _animator.SetFloat("Vertical", _verticalAxis);
+
+                switch(_horizontalAxis)
+                {
+                    case > 0:
+                        _playerRotate = PlayerRotates.Right;
+                        break;
+
+                    case < 0:
+                        _playerRotate = PlayerRotates.Left;
+                        break;
+                }
+
+                switch (_verticalAxis)
+                {
+                    case > 0:
+                        _playerRotate = PlayerRotates.Up;
+                        break;
+
+                    case < 0:
+                        _playerRotate = PlayerRotates.Down;
+                        break;
+                }
+            }
+
+            else if (_horizontalAxis == 0 || _verticalAxis == 0)
+            {
+                _animator.SetBool("isWalking", false);
+
+                switch (_playerRotate)
+                {
+                    case PlayerRotates.Up:
+                        _animator.SetFloat("Vertical", 1);
+                        break;
+
+                    case PlayerRotates.Down:
+                        _animator.SetFloat("Vertical", -1);
+                        break;
+
+                    case PlayerRotates.Right:
+                        _animator.SetFloat("Horizontal", 1);
+                        break;
+
+                    case PlayerRotates.Left:
+                        _animator.SetFloat("Horizontal", -1);
+                        break;
+
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                _currentSpeed *= 2;
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                _currentSpeed = _normalSpeed;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            _horizontalAxis = Input.GetAxisRaw("Horizontal");
+            _verticalAxis = Input.GetAxisRaw("Vertical");
+
+            Vector2 direction = new Vector2(_horizontalAxis, _verticalAxis);
+
+            _rigidbody.velocity = direction.normalized * _currentSpeed;
+        }
+    }
+}
