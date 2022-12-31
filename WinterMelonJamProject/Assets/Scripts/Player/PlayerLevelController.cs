31db91a13b3f93b56cb977de53c086a1e2f6c6e1
Richa77777 <121635433+Richa77777@users.noreply.github.com>
2022-12-31 17:10:25 +0300

    Update
    Update

--- WinterMelonJamProject/Assets/Scripts/Player/PlayerLevelController.cs
+++ WinterMelonJamProject/Assets/Scripts/Player/PlayerLevelController.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class PlayerLevelController : MonoBehaviour
+namespace Player
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerLevelController : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private int _currentLevel = 1;
+        [SerializeField] private TextMeshProUGUI _text;
+        public int CurrentLevel { get { return _currentLevel; } }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private Player.PlayerMoneyController _moneyController;
+
+        private void Start()
+        {
+            _text.text = _currentLevel.ToString();
+
+            _moneyController = gameObject.GetComponent<PlayerMoneyController>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Q))
+            {
+                GlobalEventManager.EventManager.LevelUp(_moneyController.Money, 4 * (_currentLevel));
+            }
+        }
+
+        public void AddLevel(int value, int cost)
+        {
+            _currentLevel += value;
+            _moneyController.AddMoney(-cost);
+            _text.text = _currentLevel.ToString();
+        }
     }
 }
